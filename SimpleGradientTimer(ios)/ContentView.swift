792c6122785e2af41fa8b3a7faee24067b8c340f
Artem <moiseev@physics.msu.ru>
2023-06-05 01:35:34 +0300

    Timer initial app

--- SimpleGradientTimer(ios)/ContentView.swift
+++ SimpleGradientTimer(ios)/ContentView.swift
@@ -1,24 +1,48 @@
-//
-//  ContentView.swift
-//  SimpleGradientTimer(ios)
-//
-//  Created by Artem on 05.06.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var vm = ViewModel()
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    private let width: Double = 250
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Text("\(vm.time)")
+                .font(.system(size: 70, weight: .medium, design: .rounded))
+                .padding()
+                .frame(width: width)
+                .background(.thinMaterial)
+                .cornerRadius(20)
+                //.overlay(RoundedRectangle(cornerRadius: 20))
+                    //.stroke(Color.gray, lineWidth: 4)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .alert("Timer done!",isPresented: $vm.showingAlert) {
+                    Button("Continue", role: .cancel) {
+                        //
+                    }
+                }
+            
+            Slider(value: $vm.minutes, in: 1...10, step: 1)
+                .padding()
+                .frame(width: width)
+                .disabled(vm.isActive)
+                .animation(.easeInOut, value: vm.minutes)
+            
+            HStack(spacing: 50) {
+                Button("Start") {
+                    vm.start(minutes: vm.minutes)
+                }
+                .disabled(vm.isActive)
+                
+                Button("Reset", action: vm.reset)
+                    .tint(.red)
+            }
+            .frame(width:width)
         }
-        .padding()
+        .onReceive(timer){ _ in
+            vm.updateCountdown()
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
