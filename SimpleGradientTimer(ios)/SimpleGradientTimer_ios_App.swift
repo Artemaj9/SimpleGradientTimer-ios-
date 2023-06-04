f16900bb2faec25c16220ed2d7a2b19dd70212f8
Artem <moiseev@physics.msu.ru>
2023-06-05 01:26:26 +0300

    Initial Commit

---
+++ SimpleGradientTimer(ios)/SimpleGradientTimer_ios_App.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleGradientTimer_ios_App.swift
+//  SimpleGradientTimer(ios)
+//
+//  Created by Artem on 05.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleGradientTimer_ios_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
